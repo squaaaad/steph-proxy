9f29ccbb3133e7f4720aa336883c717342b289c7
Stacy Lin <stayclin@gmail.com>
2018-03-10 12:44:12 -0800

    added ReadMe and made changes for Docker and EC2 deployment

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# proxy-server-stacy
+# WeGot - Proxy Server
+
+> Welcome to the WeGot food review website. This web application consists of four services - photo gallery, overview, map and side bar, and recommendations.
+
+## Related Projects
+
+  - https://github.com/bamboo-connection/photo-gallery
+  - https://github.com/bamboo-connection/overview
+  - https://github.com/bamboo-connection/map-side-bar
+  - https://github.com/bamboo-connection/recommendations
+
+## Table of Contents
+
+1. [Usage](#Usage)
+1. [Requirements](#requirements)
+1. [Development](#development)
+
+## Usage
+
+> 
+* __Note:__ Nodemon should be installed globally.
+* Open a terminal instance for every service and one for the proxy.
+* Follow the steps in each module's ReadMe to set up the respective service.
+* Run 'npm start' for the proxy.
+* In a browser navigate to localhost:3000
+
+## Requirements
+
+An `nvmrc` file is included if using [nvm](https://github.com/creationix/nvm).
+
+- npm
+- Node
+- Jest
+- Webpack
+- MongoDB
+
+## Development
+
+### Installing Dependencies
+
+From within the root directory:
+
+```sh
+npm install -g webpack
+npm install
+```
+
--- server.js
+++ server.js
@@ -6,25 +6,25 @@ const port = process.env.PORT || 3000;
 const request = require('request');
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/restaurants/:id', function (req, res) {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-});
-
-app.get('/api/restaurants/:id/gallery', function (req, res) {
-  res.redirect(`http://127.0.0.1:3001/api/restaurants/${req.params.id}/gallery`)
-});
-app.get('/api/restaurants/:id/overview', function (req, res) {
-  res.redirect(`http://127.0.0.1:3002/api/restaurants/${req.params.id}/overview`)
-});
-app.get('/api/restaurants/:id/sidebar', function (req, res) {
-  res.redirect(`http://127.0.0.1:3003/api/restaurants/${req.params.id}/sidebar`)
+app.get('/', (req, res) => {
+  res.redirect('/restaurants/ChIJUcXYWWGAhYARmjMY2bJAG2s/');
 });
 
-app.get('/api/restaurants/:id/recommendations', function (req, res) {
-  // request(`http://127.0.0.1:3004/api/restaurants/${req.params.id}/recommendations`).pipe(res)
-  res.redirect(`http://127.0.0.1:3004/api/restaurants/${req.params.id}/recommendations`)
+app.use('/restaurants/:id', express.static(path.join(__dirname, 'public')));
+
+app.get('/api/restaurants/:id/gallery', (req, res) => {
+  res.redirect(`http://13.57.148.57/api/restaurants/${req.params.id}/gallery`)
+});
+app.get('/api/restaurants/:id/overview', (req, res) => {
+  res.redirect(`http://184.169.248.150/api/restaurants/${req.params.id}/overview`)
+});
+app.get('/api/restaurants/:id/sidebar', (req, res) => {
+  res.redirect(`http://54.177.233.239/api/restaurants/${req.params.id}/sidebar`)
+});
+app.get('/api/restaurants/:id/recommendations', (req, res) => {
+  res.redirect(`http://52.89.102.101/api/restaurants/${req.params.id}/recommendations`)
 });
 
 app.listen(port, () => {
